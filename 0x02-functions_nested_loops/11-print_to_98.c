8ab2ea9659e423b63dd584c932b60fe9069413c0
Prince Aboagye <aboagyeprince717@gmail.com>
2023-02-22 20:28:14 -0800

    Print all natural numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,3 +9,40 @@
  *
  * Return: 0 if success
  */
+
+void print_to_98(int n)
+{
+	if (n == 98)
+	{
+		printf("%d\n", n);
+	}
+
+	if (n > 98)
+	{
+		for (n = n; n > 97; n--)
+		{
+			if (n == 98)
+			{
+				printf("%d\n", n);
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
+		}
+	}
+	if (n < 98)
+	{
+		for (n = n; n < 99; n++)
+		{
+			if (n != 98)
+			{
+				printf("%d, ", n);
+			}
+			else
+			{
+				printf("%d\n", n);
+			}
+		}
+	}
+}
