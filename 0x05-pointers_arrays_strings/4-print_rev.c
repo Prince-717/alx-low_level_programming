a7691d54462f7bfe9716d8296d6a0d81de547a5d
Prince Aboagye <aboagyeprince717@gmail.com>
2023-02-27 14:19:29 -0800

    Print string in reverse order

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - Write a function that prints a string in reverse
+ *
+ * @s: Parameter
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+	int li; /* Index of '\0' character in the string */
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	li = i;
+
+	printf("%d", li);
+
+	while (li >= 0)
+	{
+		_putchar(s[li]);
+
+		li--;
+	}
+
+	_putchar('\n'); /* Print new line */
+}
