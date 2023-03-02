f136c004596492eede8398399745ed101a19a2fc
Prince Aboagye <aboagyeprince717@gmail.com>
2023-03-02 14:07:23 -0800

    Capitalize all words

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,20 +1,28 @@
+#include "main.h"
+
 /**
  * cap_string - capitalizes all words of a string
  * @s: string to capitalize
  *
  * Return: pointer to the capitalized string
  */
+
 char *cap_string(char *s)
 {
-	int i;
-	char separators[] = " \t\n,;.!?\"(){}";
+	int i = 0, cap_next = 1;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (s[i] != '\0')
 	{
-		if (s[0] >= 'a' && s[0] <= 'z')
-			s[0] -= 32;
-		if (strchr(separators, s[i]) && s[i + 1] >= 'a' && s[i + 1] <= 'z')
-			s[i + 1] -= 32;
+		if (cap_next && (s[i] >= 'a' && s[i] <= 'z'))
+			s[i] -= 32;
+		cap_next = 0;
+		if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' ||
+				s[i] == ',' || s[i] == ';' || s[i] == '.' ||
+				s[i] == '!' || s[i] == '?' || s[i] == '\"' ||
+				s[i] == '(' || s[i] == ')' || s[i] == '{' ||
+				s[i] == '}')
+			cap_next = 1;
+		i++;
 	}
 	return (s);
 }
