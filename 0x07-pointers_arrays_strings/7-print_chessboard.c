4b61b9e873c32f8bfe1f66505e2a01853dd959dc
Prince Aboagye <aboagyeprince717@gmail.com>
2023-03-06 16:54:25 -0800

    Print chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Print the chess board.
+ *
+ * @a: Pointer to an array of 8 characters.
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i = 0;
+	int j = 0;
+
+	while (i < 8)
+	{
+		while (j < 8)
+		{
+			_putchar(a[i][j]);
+
+			j++;
+		}
+
+		_putchar('\n');
+
+		j = 0;
+
+		i++;
+	}
+}
