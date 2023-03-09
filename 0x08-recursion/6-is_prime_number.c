f3e905286a712671ce247dba06d9e978461db9d0
Prince Aboagye <aboagyeprince717@gmail.com>
2023-03-09 08:31:00 -0800

    Return 1 if input integer is a prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,19 +1,29 @@
-/**
- * is_prime_number - Return 1 if input is prime number.
- *
- * @n: Input integer parameter.
- *
- * Return: 1 if input is prime else 0 if otherwise.
- */
+#include "main.h"
 
+/**
+ * is_prime - Check if a number is prime or not.
+ * @n: The number to check.
+ * @i: The divisor to check against.
+ *
+ * Return: 1 if the number is prime, 0 otherwise.
+ */
+int is_prime(int n, int i)
+{
+	if (n <= 1)
+		return (0);
+	else if (i == 1)
+		return (1);
+	else
+		return ((n % i == 0) ? 0 : is_prime(n, i - 1));
+}
+
+/**
+ * is_prime_number - Check if a number is prime or not.
+ * @n: The number to check.
+ *
+ * Return: 1 if the number is prime, 0 otherwise.
+ */
 int is_prime_number(int n)
 {
-	if (n % 2 != 0)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0 * is_prime_number(n + 1));
-	}
+	return (is_prime(n, n / 2));
 }
