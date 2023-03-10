bda96ffca4daeb326abe6e01e246654062a30a80
Prince Aboagye <aboagyeprince717@gmail.com>
2023-03-10 06:56:38 -0800

    Create the static library libmy.a

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,14 @@
+/**
+ * _memset - Set memory
+ *
+ * @s: first parameter
+ * @b: second parameter
+ * @n: third paramentr
+ *
+ * Return: Character
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+
+}
