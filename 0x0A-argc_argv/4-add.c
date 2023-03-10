b132759a93187be0d4c5470d002980d88eda724c
Prince Aboagye <aboagyeprince717@gmail.com>
2023-03-10 15:29:06 -0800

    Add positive numbers and print result

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point for program.
+ *
+ * @argc: Number of arguments.
+ * @argv: Array of argumnets.
+ *
+ * Return: 0 on success.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+	int asci;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		asci = (int) *argv[i];
+
+		if (asci < 48 || asci > 57)
+		{
+			printf("Error\n");
+
+			break;
+		}
+
+		sum += atoi(argv[i]);
+
+	}
+
+	if (i == argc && argc > 1)
+	{
+		printf("%d\n", sum);
+	}
+
+	return (0);
+}
