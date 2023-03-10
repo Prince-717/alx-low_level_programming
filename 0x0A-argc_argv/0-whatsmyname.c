70aa220a831f37ba2c512bce1a56b064ab481f99
Prince Aboagye <aboagyeprince717@gmail.com>
2023-03-10 13:53:26 -0800

    Print program name using argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point for program that prints its name.
+ *
+ * @argc: Number of arguments passed to the program.
+ * @argv: Array of arguments passed to the program.
+ *
+ * Return: 0 on success.
+ */
+
+int main(int argc, char *argv[])
+{
+	argc + 0;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
