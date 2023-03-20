19ee506c556becc76bc3cdc5316685b079f496fe
Prince Aboagye <aboagyeprince717@gmail.com>
2023-03-20 16:17:13 -0700

    Free memory allocated to dog structures

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - Frees the memory of dog struct.
+ * @d: dog to free
+ */
+void free_dog(dog_t *d)
+{
+	(d != NULL)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
+}
