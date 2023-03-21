7aacb2c555681f68415be67c859819fd596a59cf
Prince Aboagye <aboagyeprince717@gmail.com>
2023-03-21 13:53:41 -0700

    Print the name of the file a C program was compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the file it was compiled from.
+ *
+ * Return: 0 on Success.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
