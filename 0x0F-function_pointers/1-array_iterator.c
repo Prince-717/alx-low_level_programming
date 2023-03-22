682917b25dbc1455e5b8c876e8b4162aafc1ed82
Prince Aboagye <aboagyeprince717@gmail.com>
2023-03-22 12:56:16 -0700

    Execute a function on array with pointer to function

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+
+/**
+ * array_iterator - Executes a function on an array with pointer to function.
+ *
+ * @array: Array definition.
+ * @size: Size of array.
+ * @action: Pointer to function.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int long i = 0;
+
+	while (i < size)
+	{
+		action(array[i]);
+
+		i++;
+	}
+}
