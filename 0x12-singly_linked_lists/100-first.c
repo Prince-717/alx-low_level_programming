98bc08d0ca42dbc82c82be3f87df78a70babc4df
Prince Aboagye <aboagyeprince717@gmail.com>
2023-03-31 16:12:33 -0700

    Print a string before executing the main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_before_main - prints a message before main function is executed
+ *
+ * Return: void
+ */
+void __attribute__((constructor)) print_before_main()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
