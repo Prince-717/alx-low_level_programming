cb15f0482f99bba61ef8c6ca8f802bc2da771886
Prince Aboagye <aboagyeprince717@gmail.com>
2023-05-26 16:48:29 -0700

    Create dynamic library that contains C functions that can be called from Python

---
+++ 0x18-dynamic_libraries/div.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int _div(int a, int b) {
+    return (a / b);
+}
---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int mod(int a, int b) {
+    return (a % b);
+}
